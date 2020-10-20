fd92befbef9f3b0bcfcd29c95805017b13a26410
vuongvgc <vuongvgc@gmail.com>
2020-10-20 23:08:27 +0700

    function  X )

--- src/copoments/Board.js
+++ src/copoments/Board.js
@@ -1,11 +1,31 @@
 import React from 'react';
 import Square from './Square';
 class Board extends React.Component {
-  clickBTN = () => {
-    console.log("click")
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(900).fill(null),
+      xIsNext: true,
+    };
+  }
+ 
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+    });
   }
   renderSquare = (i) => {
-     return( <Square key={i} onClick={this.clickBTN} value={i}/>)
+     return( 
+     <Square  key={i} 
+              onClick={() => this.handleClick(i)} 
+              value={this.state.squares[i]}
+              hienthi = {i}
+              />
+              
+     )
   }
   renderRow = (n) => {
     let rowsSquare = []
@@ -30,4 +50,7 @@ class Board extends React.Component {
   }
 
   }
+
+
+  
 export default Board;
