934a2221a968cbb03e8019a208fc3dbeb6bce2c3
vuongvgc <vuongvgc@gmail.com>
2020-10-22 21:05:43 +0700

    add clock

--- src/copoments/Clock/Clock.js
+++ src/copoments/Clock/Clock.js
@@ -1,10 +1,51 @@
 import React from 'react';
-function Clock(props) {
+class Clock extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      stopGame: false,
+      minute: 0,
+      second: 0,
+    }
+  }
+  componentDidMount() {
+    this.timerID = setInterval(
+      () => this.tick(),
+      50);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+  tick() {
+    let {stopGame, minute, second} = this.state
+    if(stopGame === false){
+      if(second < 60){
+        second += 1
+      }else if(second === 60){
+        minute += 1
+        second = 0
+        if(minute === 3){
+            stopGame = true
+        }
+      }
+    }
+    
+    console.log(minute)
+    this.setState({
+      stopGame: stopGame,
+      minute: minute,
+      second: second
+    })
+  }
+  render(){
+    const {minute, second} = this.state
     return (
       <div>
-        <h1>Time To Player 20:00</h1>
-        <h2>00 - 00</h2>
+        <h1>Time To Player </h1>
+        <h2>{minute}:{second}</h2>
       </div>
     );
+  }  
 }
 export default Clock;
